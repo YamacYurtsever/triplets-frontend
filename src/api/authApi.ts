05b0530f69ed57db222c7140f63cbd3a00fb3a8e
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-12-01 15:17:59 +1100

    Updated auth api

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,21 +3,21 @@ import requestHelper from "./api";
 const requestAuthRegister = async (name: string, email: string, password: string) => {
   const headers = {};
   const payload = { name, email, password };
-  const data =  await requestHelper('POST', '/auth/register', headers, payload);
+  const data = await requestHelper('POST', '/auth/register', headers, payload);
   return data;
 };
 
 const requestAuthLogin = async (email: string, password: string) => {
   const headers = {};
   const payload = { email, password };
-  const data =  await requestHelper('POST', '/auth/login', headers, payload);
+  const data = await requestHelper('POST', '/auth/login', headers, payload);
   return data;
 };
 
 const requestAuthLogout = async (token: string) => {
   const headers = { token };
   const payload = {};
-  const data =  await requestHelper('POST', '/auth/logout', headers, payload);
+  const data = await requestHelper('POST', '/auth/logout', headers, payload);
   return data;
 };
 
