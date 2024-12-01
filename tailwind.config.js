430a0eb8e2e82eabae98360700f81560af9bcb56
Yamac Yurtsever <z5549363@ad.unsw.edu.au>
2024-12-01 14:49:51 +1100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,25 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'auth-bg': "url('/src/assets/triplets_castle.png')",
+        'castle': "url('/src/assets/triplets_castle.png')",
+      },
+      spacing: {
+        'auth-card-h': '500px',
+        'auth-card-w': '800px',
+      },
+      colors: {
+        primary: {
+          default: '#954dc9',
+          light: '#954dc950',
+        },
+        secondary: {
+          default: '#e3ac40',
+          light: '#e3ac4050',
+        },
+        neutral: {
+          default: '#343236',
+          light: '#34323650',
+        },
       },
     },
   },
